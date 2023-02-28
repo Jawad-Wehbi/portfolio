b796665e19dd33f3a1d2288c4efc62b514c8e5c1
Jawad-Wehbi <jawad.a.wehbi@gmail.com>
2023-02-28 10:48:48 +0200

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+	imagebaseurl: 'https://github.com/Jawad-Wehbi',
+	name: 'Jawad Wehbi',
+	role: 'Full-stack Web Developer',
+	linkedinId: ' Jawad Wehbi',
+	skypeid: 'Your skypeid',
+	roleDescription:
+		'Motivated Web Developer and a lifelong learner. I keep abreast of the latest technology news. Always searching for self-development and improvement.',
+	socialLinks: [
+		{
+			name: 'linkedin',
+			url: 'https://www.linkedin.com/in/jawad-wehbi/',
+			className: 'fa fa-linkedin'
+		},
+		{
+			name: 'github',
+			url: 'https://github.com/Jawad-Wehbi',
+			className: 'fa fa-github'
+		}
+	],
+	aboutme:
+		"Being always passionate about technology, I chose the path of mechanical engineering during my university years and graduated with a bachelor's Degree in 2022.My journey in the field of Web Development didn't start long ago, but I sure got a long way since then. And as a current Junior Fullstack Web Developer, I look forward to the next position that will definitely align with my continuous learning journey.I have a keen interest in emerging web development technologies. During my free time, I attend the gym and practice my boxing skills, in addition to off-road driving and nature exploration.",
+	address: 'Beirut,Lebanon',
+	phone: '+961 71001455',
+	email: 'jawad.a.wehbi@gmail.com',
+
+	education: [
+		{
+			UniversityName: 'Jwaya University College of Technology',
+			specialization: 'Bachelor of Science in Mechanical Engineering',
+			MonthOfPassing: 'Jan',
+			YearOfPassing: '2023'
+		}
+	],
+	certification: [
+		{
+			CompanyName: 'SE Factory',
+			specialization: 'Foundations of Computer Sceince',
+			MonthOfLeaving: 'Aug',
+			YearOfLeaving: '2022',
+			Achievements:
+				'Intensive Foundations of Computer Science learning Program covering Python,Control Flow, and Data Structure over six weeks of lectures, and projects.'
+		},
+		{
+			CompanyName: 'SE Factory',
+			specialization: 'Full-stack Web Development',
+			MonthOfLeaving: 'Nov',
+			YearOfLeaving: '2022',
+			Achievements:
+				'Intensive full-stack web development program covering cloud architecture, front- and back-end development, source control, and web application security over 14 weeks of lectures, workshops, and projects.'
+		},
+		{
+			CompanyName: 'AWS',
+			specialization: 'AWS Certified Cloud Practitioner',
+			MonthOfLeaving: 'Feb',
+			YearOfLeaving: '2023',
+			Achievements: ''
+		}
+	],
+	skillsDescription: 'Main Skills',
+	skills: [
+		{
+			skillname: 'HTML5'
+		},
+		{
+			skillname: 'CSS'
+		},
+		{
+			skillname: 'React'
+		},
+		{
+			skillname: 'Nodejs'
+		},
+		{
+			skillname: 'Laravel'
+		}
+	],
+	portfolio: [
+		{
+			name: 'Tech-It',
+			description: 'Pre-employment testing and employee assessment Website.',
+			imgurl: 'images/Tech-It.png'
+		}
+	],
+	quotes: [
+		{
+			description:
+				'Learning to write programs stretches your mind, and helps you think better, creates a way of thinking about things that I think is helpful in all domains.',
+			name: 'Bill Gates'
+		},
+		{
+			description:
+				'The more I use a piece of code, the more confident and familiar I become with it, which in turn significantly speeds up my development time.',
+			name: 'Robert Duchnik'
+		}
+	]
+};
+
+export default resumeData;
